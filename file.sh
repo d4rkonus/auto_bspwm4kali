ed31b3db9d8bbdf01fc80670f7c73800bee084a3
d4rkonus <d4rkonus@gmail.com>
2025-10-29 20:25:35 +0000

    z

--- file.sh
+++ file.sh
@@ -24,6 +24,39 @@ ruta=$(pwd)
 
 tput civis
 
-echo -e "\nInstalling system dependencies ...\n"
-sudo apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
 
+echo -e "${yellowColour}Installing all environment dependencies...${endColour}"
+sudo apt install -y \
+        build-essential git vim cmake cmake-data pkg-config python3-sphinx \
+        xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev \
+        libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev \
+        libxcb-shape0-dev libcairo2-dev libxcb-composite0-dev python3-xcbgen \
+        xcb-proto libxcb-image0-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev \
+        libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev \
+        meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
+        libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev \
+        libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev \
+        uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev \
+        kitty feh scrot scrub rofi xclip bat locate ranger neofetch wmname acpi bspwm sxhkd imagemagick \
+    > /dev/null 2>&1
+
+mkdir ~/gittemp
+
+cd ~/gittemp
+git clone --recursive https://github.com/polybar/polybar
+git clone https://github.com/ibhagwan/picom.git
+
+cd ~/gittemp/polybar
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
+
+cd ~/github/picom
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+
+tput cnorm
